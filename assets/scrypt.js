54c13bc9bad4b98d45651cb33c3e0c32a6de22a4
nico17022001 <nico17022001boolean@gmail.com>
2023-03-17 14:07:38 +0100

    scomposto-il-problema

---
+++ assets/scrypt.js
@@ -0,0 +1,20 @@
+/*
+Consegna
+
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito23
+
+*/
+
+/*
+Passaggi
+
+1)Chiedere all'utente il nome e salvarlo  
+2)Collegare il nome all'utente id per renderlo visibile 
+3)Chiedere all'utente il cognome e salvarlo 
+4)Collegare il cognome all'utente id per renderlo visibile 
+5)Chiedere all'utente il suo colore preferito e salvarlo 
+6)Collegare il cognome all'utente id per renderlo visibile 
+*/
